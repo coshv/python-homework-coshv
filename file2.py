9f37af1406fb0f45235875ec0ee705018e5e118e
coshv <a01056674473@gmail.com>
2024-06-16 17:52:21 +0900

    숙제 2번

---
+++ file2.py
@@ -0,0 +1,22 @@
+n=float(input('1 실수를 입력'))
+n2=float(input('2 실수를 입력'))
+sum = n+n2
+print('{}와{}의 합은 {} 입니다'.format(n,n2,sum))
+
+n=input('1~12 사이의 월을 입력하시오>>>')
+january=31
+feb = 28
+march = 30
+aprill =  29
+May = 31
+June = 31
+July = 30
+August = 27
+setember = 30
+october = 31
+Novmeber= 31
+December = 31
+li = []
+print('{}월은 {}일까지 있습니다'.format(n,))
+
+
